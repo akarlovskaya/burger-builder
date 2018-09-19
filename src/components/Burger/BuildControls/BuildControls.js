5b8acf03ee28d70761c3460f9b93de4cd728f6f0
Anna <karlovskaya@gmail.com>
2018-09-19 14:50:57 -0700

    add control buttons

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
+
+const controls = [
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' }
+
+];
+
+export const buildControls = (props) => {
+  return (
+      <div className={classes.BuildControls}>
+          { controls.map( controlItem => {
+              return <BuildControl key={controlItem.label} label={controlItem.label}/>
+          })
+          }
+      </div>
+  );
+}
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,26 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
     // to extract keys as an array from Ingredients object use Object.keys()
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
     // iterate through array of key strings ['salad', 'meat' etc]
         .map( ingredientKey => {
             // transform a key string into array based on value number
             // create a new Array and pass Value number from a Key, ex. Array(2)
             return [...Array(props.ingredients[ingredientKey])]
-            // execute map() on each array and applay jxs
+            // execute map() on each array and apply jxs
             .map((_, i) => {
                 return <BurgerIngredient key={ingredientKey + i} type={ingredientKey}/>
             });
-        });
+        })
+        // create one array with all ingredients in it
+        .reduce( (arr, el) => {
+            return arr.concat(el);
+        }, []);
+
+        // show text if there is no ingredients
+        if ( transformedIngredients.length === 0 ) {
+            transformedIngredients = "Please choose ingredients";
+        }
 
     return (
         <div className={classes.Burger}>
